acf646294aa0748cbfe95594f75d26227875a698
longg2 <longg2@mcmaster.ca>
2022-01-28 11:55:42 -0500

    Beast Trees Plotted
    	• Beast Trees are *nearl* complete
    		- Realized that I forgot to account for invariant
    		  sites..........
    	• Ready for a renewed P/A analysis as I had left two assemblies
    	  in the calculation

--- Phylogenies/Beast/PhylogenyPlottingBeast.R
+++ Phylogenies/Beast/PhylogenyPlottingBeast.R
@@ -17,7 +17,7 @@ colour <- c('#e6194b', '#3cb44b', '#ffe119', '#4363d8', '#f58231', '#911eb4', '#
 '#bcf60c', '#fabebe', '#008080', '#e6beff', '#9a6324', '#fffac8', '#800000', '#aaffc3',
 '#808000', '#ffd8b1', '#000075', '#808080', '#f0f0ff', '#000000')
 
-ann_colors <- list(ST = c("5" = colour[10], "7" = colour[15], "8" = colour[3], "9" = colour[9], "10" = colour[6], "11" = colour[17], "12" = colour[8], "39" = colour[13], "40" = colour[2], "41" = colour[16], "42" = colour[7], "43" = colour[4], "71" = colour[11], "88" = colour[12], "102" = colour[19], "Ancient" = colour[1], "NF" = colour[20], "Reference" = colour[22], "Kay" = colour[5]))
+ann_colors <- list(ST = c("5" = colour[10], "7" = colour[15], "8" = colour[3], "9" = colour[9], "10" = colour[6], "11" = colour[17], "12" = colour[8], "39" = colour[13], "40" = colour[2], "41" = colour[16], "42" = colour[7], "43" = colour[4], "71" = colour[11], "88" = colour[12], "102" = colour[19], "Corsini" = colour[1], "NF" = colour[20], "Reference" = colour[22], "Geridu" = colour[5]))
 
 LocationID <- function(Sample){
 	ifelse(grepl("\\dTE\\d|\\dCB\\d|\\dIS\\d|Kay|JessSample", Sample), "Italy",
@@ -29,7 +29,7 @@ LocationID <- function(Sample){
 }
 
 # Basic Tree
-tree <- read.beast(file = "BeastResults/ST11Strict/test.nexus")
+tree <- read.beast(file = "BeastResults/ExponentialST11/ST11KayIncludedExponential.nexus")
 Tiplabels <- tree@phylo$tip.label
 
 ####################################################
@@ -37,15 +37,15 @@ Tiplabels <- tree@phylo$tip.label
 ####################################################
 STData <- read.delim("../MLSTResults.txt")[,1:2]
 rownames(STData) <- STData$Sample
-STData["JessSample",] <- list(Sample = "JessSample", ST = "Ancient")
+STData["JessSample",] <- list(Sample = "JessSample", ST = "Corsini")
 STData["Reference",] <- list(Sample = "Reference", ST = "Reference")
-STData["KayBMel",] <- list(Sample = "KayBMel", ST = "Kay")
+STData["KayBMel",] <- list(Sample = "KayBMel", ST = "Geridu")
 
 STData<- STData[Tiplabels,]
 STData <- STData %>% mutate(ST = gsub("[^[:alnum:] ]","", ST))
 
 # Let's filter the ann_colors list so that only the STs present in the phylogeny are used
-ann_colors$ST <- ann_colors$ST[unique(STData$ST)]
+ann_colors$ST <- ann_colors$ST[mixedsort(unique(STData$ST))]
 
 # Let's see if we can include some geographic information
 STData <- STData %>% mutate(Country = LocationID(Sample))
@@ -55,9 +55,9 @@ p1 <- ggtree(tree, right = T, mrsd = "2017-01-01") %<+% STData + #This is for th
 	theme_tree2() +
 	geom_rootedge(rootedge = 50) +
 	geom_range("height_0.95_HPD", colour = "red", size = 0.75, alpha = 0.75) +
-	geom_tippoint(aes(colour = ST, shape = Country), size = 3) +
+	geom_tippoint(aes(colour = ST), size = 2) +
 	scale_color_manual(values = ann_colors$ST, name = "Sequence Type") +
-	scale_shape_manual(values = c(16:18,6:10)) +
+	#scale_shape_manual(values = c(16:18,6:10)) +
 	new_scale_color() +
        	geom_nodepoint(aes(color = ifelse(posterior < 0.5, NA,
 						 ifelse(posterior >= 0.5 & posterior < 0.9, "Fifty", "Ninety"))),
@@ -74,6 +74,6 @@ p1 <- ggtree(tree, right = T, mrsd = "2017-01-01") %<+% STData + #This is for th
 
 	p1
 
-ggsave(plot = p1, "ST11BeastWithKay.pdf", width = 12, height = 9)
+ggsave(plot = p1, "ST11GenomeExponential.pdf", width = 12, height = 9)
 
 decimal_date(ymd("1394-08-14"))
